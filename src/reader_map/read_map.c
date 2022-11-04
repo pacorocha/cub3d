402440a5d3fb49bb616b449c2efef8dc96b29509
Danimdf <danimdf360@gmail.com>
2022-11-04 02:42:08 +0100

    refactor: beginning of separator archives

---
+++ src/reader_map/read_map.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 02:37:15 by coder             #+#    #+#             */
+/*   Updated: 2022/11/04 02:37:57 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	read_map(t_data *data)
+{
+	int content;
+	int	gnl;
+	int	i;
+
+	data->cub = lines(data->file, data);
+	content = open(data->file, O_RDONLY);
+	gnl = 1;
+	i = 0;
+	while (gnl)
+		gnl = get_next_line(content, &data->cub[i++]);
+	data->cub[i] = NULL;
+	close(content);
+}
+
+char	**lines(char *file, t_data *data)
+{
+	int		fd;
+	int		read_line;
+	char	c;
+
+	fd = open(file, O_RDONLY);
+	if (!fd || fd < 0)
+		print_error("Error!");
+	if (!check_end_of_file(file, "cub"))
+		print_error("Error, cub");
+	data->nb_rows = 1;
+	read_line = 1;
+	while (read_line)
+	{
+		read_line = read(fd, &c, 1);
+		if (read_line < 0)
+			return (NULL);
+		if (c == '\n')
+			data->nb_rows++;
+	}
+	close(fd);
+	data->cub = (char **)ft_calloc(data->nb_rows + 1, sizeof(char *));
+	return (data->cub);
+}
